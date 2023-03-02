b8b8e05d3bb98f9135335d79fce8302e501cb0c7
Ry Work <spucti19@gmail.com>
2023-03-02 17:21:49 -0500

    got path set up and folders being made at user selected path. Notices for if duplicate folders are trying to be made. blank box message

---
+++ folder-creator-manager/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace folder_creator_manager
+{
+    partial class FrmFolderCreator3000
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateFolder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnChooseFldrPath = new System.Windows.Forms.Button();
+            this.txtCurrentDirBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCreateFolder
+            // 
+            this.btnCreateFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateFolder.Location = new System.Drawing.Point(12, 274);
+            this.btnCreateFolder.Name = "btnCreateFolder";
+            this.btnCreateFolder.Size = new System.Drawing.Size(151, 40);
+            this.btnCreateFolder.TabIndex = 0;
+            this.btnCreateFolder.Text = "Create Folder";
+            this.btnCreateFolder.UseVisualStyleBackColor = true;
+            this.btnCreateFolder.Click += new System.EventHandler(this.btnCreateFolder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder Name ";
+            // 
+            // txtFolderName
+            // 
+            this.txtFolderName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFolderName.Location = new System.Drawing.Point(12, 242);
+            this.txtFolderName.Name = "txtFolderName";
+            this.txtFolderName.Size = new System.Drawing.Size(151, 26);
+            this.txtFolderName.TabIndex = 2;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // btnChooseFldrPath
+            // 
+            this.btnChooseFldrPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChooseFldrPath.Location = new System.Drawing.Point(12, 24);
+            this.btnChooseFldrPath.Name = "btnChooseFldrPath";
+            this.btnChooseFldrPath.Size = new System.Drawing.Size(196, 40);
+            this.btnChooseFldrPath.TabIndex = 0;
+            this.btnChooseFldrPath.Text = "Choose Folder Path";
+            this.btnChooseFldrPath.UseVisualStyleBackColor = true;
+            this.btnChooseFldrPath.Click += new System.EventHandler(this.btnChooseFldrPath_Click);
+            // 
+            // txtCurrentDirBox
+            // 
+            this.txtCurrentDirBox.Enabled = false;
+            this.txtCurrentDirBox.Location = new System.Drawing.Point(215, 24);
+            this.txtCurrentDirBox.Multiline = true;
+            this.txtCurrentDirBox.Name = "txtCurrentDirBox";
+            this.txtCurrentDirBox.Size = new System.Drawing.Size(316, 40);
+            this.txtCurrentDirBox.TabIndex = 3;
+            // 
+            // FrmFolderCreator3000
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCurrentDirBox);
+            this.Controls.Add(this.txtFolderName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChooseFldrPath);
+            this.Controls.Add(this.btnCreateFolder);
+            this.Name = "FrmFolderCreator3000";
+            this.Text = "Folder Creator 3000";
+            this.Load += new System.EventHandler(this.FrmFolderCreator3000_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolderName;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnChooseFldrPath;
+        private System.Windows.Forms.TextBox txtCurrentDirBox;
+    }
+}
+
---
+++ folder-creator-manager/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace folder_creator_manager
+{
+    public partial class FrmFolderCreator3000 : Form
+    {
+        public FrmFolderCreator3000()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmFolderCreator3000_Load(object sender, EventArgs e)
+        {
+            if (txtCurrentDirBox.Text == "") txtCurrentDirBox.Text = "Please choose path";
+        }
+        FolderBrowserDialog folderDlg1 = new FolderBrowserDialog();
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        // Setting Current Directory
+        private void btnChooseFldrPath_Click(object sender, EventArgs e)
+        {
+         
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            DialogResult result = folderDlg1.ShowDialog();
+            txtCurrentDirBox.Text = folderDlg1.SelectedPath;
+            Directory.SetCurrentDirectory($@"{folderDlg1.SelectedPath}");
+        }
+
+        private void btnCreateFolder_Click(object sender, EventArgs e)
+        {
+            if (txtFolderName.Text == "")
+            {
+                MessageBox.Show("Please input a PO# for the folder", "Notice!");
+                return;
+            }
+
+            else if (Directory.Exists($"{txtFolderName.Text}"))
+            {
+                MessageBox.Show("A folder with that name Already Exists!", "Alert!");
+                return;
+            }
+
+            else if (!Directory.Exists($"{txtFolderName.Text}"))
+            {
+
+                Directory.CreateDirectory($"{txtFolderName.Text}");
+                txtFolderName.Text = "";
+            }
+        }
+    }
+}
